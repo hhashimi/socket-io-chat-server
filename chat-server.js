b5038e978ade562eb69f889f7d67e6075d852d1f
Hamid <hamid@rumie.org>
2021-10-25 06:54:42 -0500

    app.yaml

--- chat-server.js
+++ chat-server.js
@@ -6,14 +6,4 @@ io.on("connection", (socket) => {
     // emit to all sockets
     io.emit("server-message", message);
   });
-
-  io.emit("server-message", {
-    text: "message from server",
-    username: "Darth Vader",
-  });
-  io.emit("server-message", { text: "hello", username: "Han Solo" });
-  io.emit("server-message", {
-    text: "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
-    username: "Banana",
-  });
 });
