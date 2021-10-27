1fdff69c056c072c01724edb5c65a56bede9168c
Hamid <hamid@rumie.org>
2021-10-27 10:45:19 -0500

    support for joining channels + online users count

--- chat-server.js
+++ chat-server.js
@@ -1,9 +1,34 @@
 var io = require("./bin/www");
 
+let onlineUsersCount = 0;
+
 io.on("connection", (socket) => {
+  onlineUsersCount++;
+  io.emit("online-users", onlineUsersCount);
+
+  // join default room
+  socket.on("join-channel", (channel) => {
+    socket.join(channel);
+  });
+
   // chat message from client
   socket.on("chat-message", (message) => {
-    // emit to all sockets
-    io.emit("server-message", message);
+    message.created = new Date().toLocaleString("en-US", {
+      weekday: "short",
+      day: "numeric",
+      year: "numeric",
+      month: "long",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+
+    // emit to all sockets in channel
+    io.to(message.channel).emit("server-message", message);
+  });
+
+  socket.on("disconnect", () => {
+    onlineUsersCount--;
+    io.emit("online-users", onlineUsersCount);
   });
 });
